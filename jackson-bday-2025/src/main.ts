adb4a995fd932e80972e69f2117b2c0338c2e21c
nwelsh <newelsh@wisc.edu>
2025-07-10 10:33:19 -0500

    image

--- jackson-bday-2025/src/main.ts
+++ jackson-bday-2025/src/main.ts
@@ -17,7 +17,7 @@ document.body.appendChild(renderer.domElement);
 // === Cube with texture ===
 const geometry = new THREE.BoxGeometry();
 const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load('/jackson-bday-2025/img/jackson.jpg');
+const texture = textureLoader.load('./public/img/jackson.jpg');
 const material = new THREE.MeshBasicMaterial({ map: texture });
 
 const cube = new THREE.Mesh(geometry, material);
